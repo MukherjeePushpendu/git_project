3430f5ef6861be2479c77127c64384d0ac78eaf3
Mukherjeepushpendu <your.email@example.com>
2025-05-09 09:30:44 +0530

    Added /submittodoitem route to store item in MongoDB

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    itemName = request.form['itemName']
+    itemDescription = request.form['itemDescription']
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['todo_db']
+    collection = db['items']
+    collection.insert_one({'itemName': itemName, 'itemDescription': itemDescription})
+    return "Item added!"
 
 if __name__ == '__main__':
     app.run(debug=True)
